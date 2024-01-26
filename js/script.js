8fa66656da8383203f92004dc0ed1082ec9e0c51
Doug Sá <79709843+darneees@users.noreply.github.com>
2024-01-26 14:10:32 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function() {
-    setTimeout(function() {
-        document.querySelector(".popup").classList.add("show");
-    }, 1100);
-});
-
-document.querySelector("#close").addEventListener("click", function() {
-    document.querySelector(".popup").classList.remove("show");
-});
